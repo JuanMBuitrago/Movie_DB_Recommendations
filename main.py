0041c509d17d42d9903bc44a1a96ccbb1b4a660d
JuanMBuitrago <91805940+JuanMBuitrago@users.noreply.github.com>
2023-08-09 22:22:42 -0500

    fixed fastapi

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 import pandas as pd
 import numpy as np
 import ast
-from fastapi import FastAPI
+import fastapi
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
